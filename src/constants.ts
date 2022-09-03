7500be9d93e4fe0b8f915a6c36bf841143b7421d
Ilan 🌝 <ilanbrianrosen@gmail.com>
2022-09-03 16:15:07 -0700

    clean up strings

--- src/constants.ts
+++ src/constants.ts
@@ -3,12 +3,12 @@ import { Chain } from "./types";
 export const API_BASE_URL = 'https://api.poprank.io';
 
 export const OPENSEA_BASE_URL = 'https://opensea.io';
-export const OPENSEA_BASE_COLLECTIONS_URL = `${OPENSEA_BASE_URL}`;
+export const OPENSEA_BASE_COLLECTIONS_URL = OPENSEA_BASE_URL;
 export const OPENSEA_BASE_NFTS_URL = `${OPENSEA_BASE_URL}/assets`;
 
 export const MAGICEDEN_BASE_URL = 'https://magiceden.io';
-export const MAGICEDEN_BASE_COLLECTIONS_URL = 'https://magiceden.io';
-export const MAGICEDEN_BASE_NFTS_URL = 'https://magiceden.io';
+export const MAGICEDEN_BASE_COLLECTIONS_URL = `${MAGICEDEN_BASE_URL}/marketplace`;
+export const MAGICEDEN_BASE_NFTS_URL = `${MAGICEDEN_BASE_URL}/item-details`;
 
 /** Holds the block explorer URL account/address lookup prefix for each chain */
 export const blockExplorerDisplayNames: Record<Chain, string> = {
