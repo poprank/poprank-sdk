460d233ea74d64a5f9ec80658fa219b02e9bd013
Ilan 🌝 <ilanbrianrosen@gmail.com>
2022-09-03 16:15:42 -0700

    remove block explorer constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
-import { Chain } from "./types";
 
 export const API_BASE_URL = 'https://api.poprank.io';
 
@@ -9,10 +8,3 @@ export const OPENSEA_BASE_NFTS_URL = `${OPENSEA_BASE_URL}/assets`;
 export const MAGICEDEN_BASE_URL = 'https://magiceden.io';
 export const MAGICEDEN_BASE_COLLECTIONS_URL = `${MAGICEDEN_BASE_URL}/marketplace`;
 export const MAGICEDEN_BASE_NFTS_URL = `${MAGICEDEN_BASE_URL}/item-details`;
-
-/** Holds the block explorer URL account/address lookup prefix for each chain */
-export const blockExplorerDisplayNames: Record<Chain, string> = {
-    ethereum: 'Etherscan',
-    polygon: 'PolygonScan',
-    solana: 'SolScan',
-};
