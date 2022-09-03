6101033d29a855ad3e0c228efcb60292a2d5c477
Ilan 🌝 <ilanbrianrosen@gmail.com>
2022-09-03 13:12:36 -0700

    remove jest + move in link formatters

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,9 @@
+import { Chain } from "./types/general";
+
 export const API_BASE_URL = 'https://api.poprank.io';
+
+export const marketplaceLinkPrefixes: Record<Chain, string> = {
+    'ethereum': 'https://opensea.io/assets',
+    'polygon': 'https://opensea.io/assets/matic',
+    'solana': 'https://magiceden.io/item-details',
+};
