78b3b7fbdd93617a1d956cb508d70b2c95d348e2
Ilan 🌝 <ilanbrianrosen@gmail.com>
2022-09-08 09:10:53 -0700

    Adding chain identifier labels

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { Chain } from "./types";
+
 export const API_BASE_URL = 'https://api.poprank.io';
 
 export const OPENSEA_BASE_URL = 'https://opensea.io';
@@ -7,3 +9,24 @@ export const OPENSEA_BASE_NFTS_URL = `${OPENSEA_BASE_URL}/assets`;
 export const MAGICEDEN_BASE_URL = 'https://magiceden.io';
 export const MAGICEDEN_BASE_COLLECTIONS_URL = `${MAGICEDEN_BASE_URL}/marketplace`;
 export const MAGICEDEN_BASE_NFTS_URL = `${MAGICEDEN_BASE_URL}/item-details`;
+
+const evmLabels = {
+    collectionId: "Contract Address",
+    tokenId: "Token ID",
+    tokenStandard: "ERC721"
+};
+
+/**
+ * Labels for collection, token, and token standards per-chain.
+ * eg: on EVM chains, we have a "Contract Address", but on Solana that's
+ * referred to as an "On-chain Collection"
+ */
+export const chainIdentifierLabels: Record<Chain, { collectionId: string; tokenId: string; tokenStandard: string; }> = {
+    ethereum: evmLabels,
+    polygon: evmLabels,
+    solana: {
+        collectionId: "On-chain Collection",
+        tokenId: "Mint Account",
+        tokenStandard: 'NonFungible'
+    }
+};
